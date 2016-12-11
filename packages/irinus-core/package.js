f99d4e54de4cb38a3fb940d8adcad73f2dac1403
fmarques55 <fmarques899@gmail.com>
2016-12-11 01:00:41 -0200

    Up project

---
+++ packages/irinus-core/package.js
@@ -0,0 +1,13 @@
+/* Information about this package */
+Package.describe({
+  // Short two-sentence summary.
+  summary: "This package is responsible of the proccess of data of the application",
+  // Version number.
+  version: "0.0.0",
+  // Optional.  Default is package directory name.
+  name: "irinus:core"
+});
+/* This defines your actual package */
+Package.onUse(function (api) {
+
+});
---
+++ packages/irinus-login/package.js
@@ -0,0 +1,13 @@
+/* Information about this package */
+Package.describe({
+  // Short two-sentence summary.
+  summary: "This package is responsible to the login of the application",
+  // Version number.
+  version: "0.0.0",
+  // Optional.  Default is package directory name.
+  name: "irinus:login"
+});
+/* This defines your actual package */
+Package.onUse(function (api) {
+
+});
