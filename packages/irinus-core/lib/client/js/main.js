630a5f3d6a6d87d97fe0711cb0649a72ab291d55
ruth <fmarques899@gmail.com>
2017-01-03 18:10:45 -0200

    Helper for getting actual date

--- packages/irinus-core/lib/client/js/main.js
+++ packages/irinus-core/lib/client/js/main.js
@@ -1,7 +1,14 @@
-Template.main.onRendered(function(){
-  $('.carousel').init();
-})
-
 Template.main.events({
 
+});
+
+Template.main.helpers({
+  printActualDate(){
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+
+    return (day + "/" + month + "/" + year);
+  }
 })
